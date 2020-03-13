c2e4ea01af5b35a4c5a099eefe63e6d854cacfbe
Diana De Leon <dianaximeo@gmail.com>
2020-03-13 17:49:33 -0600

    Baby form styles finished

---
+++ daycare/src/components/AddBaby/addBaby.js
@@ -0,0 +1,41 @@
+import {v4 as uuidv4} from 'uuid';
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+
+import * as actions from '../../actions/babies';
+import './styles.css';
+
+const BabyForm = ({ onSubmit }) => {
+    const [babyName, changeBabyName] = useState('');
+    const [babyLastname, changeBabyLastname] = useState('');
+    return(
+        <div className="babyForm">
+            <input className="nameInput"
+                type="text"
+                placeholder="Enter name"
+                value={babyName}
+                onChange={e => changeBabyName(e.target.value)}
+            />
+            <input className="nameInput"
+                type="text"
+                placeholder="Enter lastname"
+                value={babyLastname}
+                onChange={e => changeBabyLastname(e.target.value)}
+            />
+            <button className="addBabyButton"
+                type="submit" onClick={() => onSubmit(babyName, babyLastname)}
+            >
+                {'Add Baby'}
+            </button>
+        </div>
+    );
+}
+
+export default connect(
+    undefined,
+    dispatch => ({
+        onSubmit(babyName, babyLastname) {
+            dispatch(actions.addBaby(uuidv4(), babyName, babyLastname));
+        },
+    }),
+)(BabyForm);
--- daycare/src/components/AddBaby/index.js
+++ daycare/src/components/AddBaby/index.js
@@ -1,41 +1,14 @@
-import {v4 as uuidv4} from 'uuid';
-import React, { useState } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
+import BabyForm from './addBaby';
 
-import * as actions from '../../actions/babies';
-import './styles.css';
 
-const BabyForm = ({ onSubmit }) => {
-    const [babyName, changeBabyName] = useState('');
-    const [babyLastname, changeBabyLastname] = useState('');
-    return(
-        <div className="babyForm">
-            <input className="nameInput"
-                type="text"
-                placeholder="Enter name"
-                value={babyName}
-                onChange={e => changeBabyName(e.target.value)}
-            />
-            <input className="nameInput"
-                type="text"
-                placeholder="Enter lastname"
-                value={babyLastname}
-                onChange={e => changeBabyLastname(e.target.value)}
-            />
-            <button className="addBabyButton"
-                type="submit" onClick={() => onSubmit(babyName, babyLastname)}
-            >
-                {'Add Baby'}
-            </button>
-        </div>
-    );
-}
+const Baby = () => (
+    <div className="form">
+        <label className="babiesLabel">{'Babies'}</label>
+        <div className="space">
+            <BabyForm />
+        </div>      
+    </div>
+  );
 
-export default connect(
-    undefined,
-    dispatch => ({
-        onSubmit(babyName, babyLastname) {
-            dispatch(actions.addBaby(uuidv4(), babyName, babyLastname));
-        },
-    }),
-)(BabyForm);
+export default Baby;
